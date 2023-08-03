596e6c6a36e53ad788c1a75fb0f0fea20743817e
vinit6626 <38699351+vinit6626@users.noreply.github.com>
2023-08-03 16:24:19 -0400

    Brand Category and Price filter implemented

--- filter.php
+++ filter.php
@@ -2,6 +2,8 @@
 require_once("db_conn.php");
 
 $brand = $_GET['brand'];
+$category = $_GET['category'];
+$sortPrice = $_GET['sortPrice'];
 
 // Modify your SQL query based on filter values
 $stmt = "SELECT s.s_id, s.shoe_name, s.product_image, s.shoe_sizes, s.gender, s.price, b.b_name as brand_name
@@ -12,11 +14,27 @@ $stmt = "SELECT s.s_id, s.shoe_name, s.product_image, s.shoe_sizes, s.gender, s.
 if ($brand !== 'all') {
   $stmt .= " WHERE b.b_id = '$brand'";
 }
-// You can add similar conditions for sorting based on $sort value
+
+if ($category !== 'all') {
+  if ($brand !== 'all') {
+    $stmt .= " AND ";
+  } else {
+    $stmt .= " WHERE ";
+  }
+  $stmt .= "s.c_id = '$category'";
+}
+
+// Add sorting based on price
+if ($sortPrice === 'low_to_high') {
+  $stmt .= " ORDER BY s.price ASC";
+} elseif ($sortPrice === 'high_to_low') {
+  $stmt .= " ORDER BY s.price DESC";
+}
 
 $result = mysqli_query($conn, $stmt);
 $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 // Return the filtered data in JSON format
 echo json_encode($products);
+
 ?>
--- filter_section.php
+++ filter_section.php
@@ -5,6 +5,10 @@ require_once("db_conn.php");
 $brandsQuery = "SELECT b_id, b_name FROM brands";
 $brandsResult = mysqli_query($conn, $brandsQuery);
 $brands = mysqli_fetch_all($brandsResult, MYSQLI_ASSOC);
+
+$categoriesQuery = "SELECT c_id, c_name FROM categories";
+$categoriesResult = mysqli_query($conn, $categoriesQuery);
+$categories = mysqli_fetch_all($categoriesResult, MYSQLI_ASSOC);
 ?>
 
 
@@ -22,6 +26,23 @@ $brands = mysqli_fetch_all($brandsResult, MYSQLI_ASSOC);
         <?php endforeach; ?>
       </select>
     </div>
+    <!-- Category Filter -->
+<div class="col-md-2">
+  <label for="category">Category:</label>
+  <select class="form-select" id="category">
+    <option value="all">All</option>
+    <?php foreach ($categories as $category): ?>
+      <option value="<?php echo $category['c_id']; ?>"><?php echo $category['c_name']; ?></option>
+    <?php endforeach; ?>
+  </select>
+</div>
+<div class="col-md-2">
+  <label for="sortPrice">Price:</label>
+  <select class="form-select" id="sortPrice">
+    <option value="low_to_high">Low to High</option>
+    <option value="high_to_low">High to Low</option>
+  </select>
+</div>
     <div class="col-md-2">
       <button type="button" class="btn btn-primary btn-block" onclick="applyFilters()">Apply Filters</button>
     </div>
@@ -31,8 +52,9 @@ $brands = mysqli_fetch_all($brandsResult, MYSQLI_ASSOC);
 
 <script>
 function applyFilters() {
-    console.log("Apply Filters button clicked");
   const brandValue = document.getElementById('brand').value;
+  const categoryValue = document.getElementById('category').value;
+  const sortPriceValue = document.getElementById('sortPrice').value;
 
   // Send AJAX request to fetch filtered data
   const xhttp = new XMLHttpRequest();
@@ -43,34 +65,45 @@ function applyFilters() {
     }
   };
 
-  xhttp.open('GET', `filter.php?brand=${brandValue}`, true);
+  xhttp.open('GET', `filter.php?brand=${brandValue}&category=${categoryValue}&sortPrice=${sortPriceValue}`, true);
   xhttp.send();
 }
+
 
 function displayFilteredData(products) {
   const productContainer = document.getElementById('productContainer');
   productContainer.innerHTML = ''; // Clear previous products
 
-  // Loop through the filtered products and create cards
-  products.forEach(product => {
-    const cardHTML = `
-      <div class="col-md-4">
-        <div class="card mb-4">
-          <img src="${product.product_image}" class="card-img-top" alt="${product.shoe_name}" style="height: 550px;">
-          <div class="card-body">
-            <h5 class="card-title">${product.shoe_name}</h5>
-            <p class="card-text">${product.brand_name} | ${product.gender} | ${product.shoe_sizes}</p>
-            <p class="card-text">Price: $${parseFloat(product.price).toFixed(2)}</p>
-            <div class="text-center">
-              <a href='item.php?s_id=${product.s_id}' class='btn btn-primary'>View this product</a>
+  // Check if there are any products to display
+  if (products.length === 0) {
+    const noResultsHTML = `
+      <div class="col-md-12 text-center">
+        <h3>No results found.</h3>
+      </div>
+    `;
+    productContainer.insertAdjacentHTML('beforeend', noResultsHTML);
+  } else {
+    // Loop through the filtered products and create cards
+    products.forEach(product => {
+      const cardHTML = `
+        <div class="col-md-4">
+          <div class="card mb-4">
+            <img src="${product.product_image}" class="card-img-top" alt="${product.shoe_name}" style="height: 550px;">
+            <div class="card-body">
+              <h5 class="card-title">${product.shoe_name}</h5>
+              <p class="card-text">${product.brand_name} | ${product.gender} | ${product.shoe_sizes}</p>
+              <p class="card-text">Price: $${parseFloat(product.price).toFixed(2)}</p>
+              <div class="text-center">
+                <a href='item.php?s_id=${product.s_id}' class='btn btn-primary'>View this product</a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    `;
+      `;
 
-    productContainer.insertAdjacentHTML('beforeend', cardHTML);
-  });
+      productContainer.insertAdjacentHTML('beforeend', cardHTML);
+    });
+  }
 }
 
 </script>
